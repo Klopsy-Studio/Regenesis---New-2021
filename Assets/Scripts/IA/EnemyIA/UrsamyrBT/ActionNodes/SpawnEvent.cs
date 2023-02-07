c807964c2e18a05fc7eb202f5ab5aa9f4e087275
JUANIG\juanig <darsodios@gmail.com>
2023-02-07 12:12:35 +0100

    Spider done, ready for testing

--- Assets/Scripts/IA/EnemyIA/UrsamyrBT/ActionNodes/SpawnEvent.cs
+++ Assets/Scripts/IA/EnemyIA/UrsamyrBT/ActionNodes/SpawnEvent.cs
@@ -9,10 +9,18 @@ public class SpawnEvent : ActionNode
     [SerializeField] List<MonsterEvent> events;
     protected override void OnStart() {
 
-        MonsterEvent eventToSpawn = events[Random.Range(0, events.Count)];
-        MonsterEvent e = owner.controller.SpawnEvent(eventToSpawn);
-        e.transform.parent = null;
-        owner.controller.battleController.timelineElements.Add(e);
+        if(events.Count == 0)
+        {
+            Debug.Log("No Event to spawn on node " + owner.nodes.IndexOf(this));
+        }
+        else
+        {
+            MonsterEvent eventToSpawn = events[Random.Range(0, events.Count)];
+            MonsterEvent e = owner.controller.SpawnEvent(eventToSpawn);
+            e.transform.parent = null;
+            owner.controller.battleController.timelineElements.Add(e);
+        }
+        
     }
 
     protected override void OnStop() {
