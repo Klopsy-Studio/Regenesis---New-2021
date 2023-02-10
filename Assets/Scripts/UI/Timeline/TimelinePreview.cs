4ee20b62d550d9af2f0645155dcd0af12abac4ee
kailuncai <kailuncai@gmail.com>
2023-02-10 17:11:37 +0100

    no message

--- Assets/Scripts/UI/Timeline/TimelinePreview.cs
+++ Assets/Scripts/UI/Timeline/TimelinePreview.cs
@@ -48,5 +48,72 @@ public class TimelinePreview : MonoBehaviour
     }
 
 
-   
+
+}
+
+
+
+// C# program to demonstrate the concept of 
+// List<T>.Sort(IComparer <T>) method
+//https://www.geeksforgeeks.org/how-to-sort-list-in-c-sharp-set-1/#m2
+
+
+class GFG : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        if (x == 0 || y == 0)
+        {
+            return 0;
+        }
+
+        // CompareTo() method
+        return x.CompareTo(y);
+
+    }
+}
+
+public class geek
+{
+
+    // Main Method
+    public static void Main()
+    {
+
+        // List initialize
+        List<int> list1 = new List<int>
+        {
+              
+            // list elements
+            1,5,6,2,4,3
+
+        };
+
+        Console.WriteLine("Original List");
+
+        foreach (int g in list1)
+        {
+
+            // Display Original List
+            Console.WriteLine(g);
+
+        }
+
+        // "gg" is the object of class GFG
+        GFG gg = new GFG();
+
+        Console.WriteLine("\nSort with a comparer:");
+
+        // use of List<T>.Sort(IComparer<T>) 
+        // method. The comparer is "gg"
+        list1.Sort(gg);
+
+        foreach (int g in list1)
+        {
+
+            // Display sorted list
+            Console.WriteLine(g);
+
+        }
+    }
 }
