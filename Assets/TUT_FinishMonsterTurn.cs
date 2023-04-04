1026bdfdc48cb524b910ea28c84ed99a8a071cb6
kailuncai <kailuncai@gmail.com>
2023-04-04 17:49:39 +0200

    TUT_Finishmonsterturn

---
+++ Assets/TUT_FinishMonsterTurn.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+[System.Serializable]
+public class TUT_FinishMonsterTurn : ActionNode
+{
+    [SerializeField] TimelineVelocity finishVelocity = TimelineVelocity.Normal;
+    protected override void OnStart()
+    {
+        owner.controller.test = false;
+    }
+
+    protected override void OnStop()
+    {
+    }
+
+    protected override State OnUpdate()
+    {
+        owner.controller.currentEnemy.timelineVelocity = finishVelocity;
+        owner.controller.currentEnemy.SetCurrentVelocity();
+        //owner.controller.battleController.ChangeState<FinishEnemyUnitTurnState>();
+        Debug.Log("PONER ESTADO TUTSHOWSLIDESTATE");
+        return State.Success;
+    }
+}
