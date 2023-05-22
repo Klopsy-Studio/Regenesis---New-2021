96fad012b43c3344bfa11a47a676280c7b40f9b3
kailuncai <kailuncai@gmail.com>
2023-05-22 09:34:48 +0200

    intento de arreglarlo

--- Assets/Scripts/UI/Camp System/Inventories/EquipmentInventoryManager.cs
+++ Assets/Scripts/UI/Camp System/Inventories/EquipmentInventoryManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class EquipmentInventoryManager : MonoBehaviour
@@ -19,27 +20,44 @@ public class EquipmentInventoryManager : MonoBehaviour
     public delegate void EquipmentButtonSlotClicked();
     public static event EquipmentButtonSlotClicked OnEquipmentButtonCliked;
 
+    //List<EquipmentSlotButton> slotButtonsList = new List<EquipmentSlotButton>();
+
     public void ButtonClicked()
     {
+
         OnEquipmentButtonCliked?.Invoke();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        CreateDisplay();
+       
         rightPanel.SetActive(false);
     }
+
+    private void OnEnable()
+    {
+        CreateDisplay();
+    }
+
 
 
     // Update is called once per frame
     //void Update()
     //{
-    //    //UpdateDisplay();
+    //    UpdateDisplay();
     //}
 
     private void CreateDisplay()
     {
+
+        //foreach (var item in slotButtonsList)
+        //{
+        //    slotButtonsList.Remove(item);
+        //    Destroy(item.gameObject);
+        //}
+       
+        consumableDisplayed.Clear();
         for (int i = 0; i < inventory.container.Count; i++)
         {
 
@@ -55,8 +73,36 @@ public class EquipmentInventoryManager : MonoBehaviour
                 slotButton.SetWeaponSlotInfo(this,weapon);
             }
 
+            //slotButtonsList.Add(slotButton);
+
         }
     }
+
+    //private void UpdateDisplay()
+    //{
+    //    for (int i = 0; i < inventory.container.Count; i++)
+    //    {
+    //        if (consumableDisplayed.ContainsKey(inventory.container[i]))
+    //        {
+    //            continue;
+    //        }
+    //        else
+    //        {
+    //            var obj = Instantiate(slotPrefab, Vector3.zero, Quaternion.identity, contentTransform);
+
+    //            var equipmentSlot = inventory.container[i];
+    //            obj.SetSlotButton(equipmentSlot);
+    //            consumableDisplayed.Add(equipmentSlot, obj);
+    //            var weapon = inventory.container[i].weapon;
+    //            if (obj.TryGetComponent(out EquipmentSlotButton slotButton))
+    //            {
+
+    //                slotButton.SetWeaponSlotInfo(this, weapon);
+    //            }
+    //        }
+    //    }
+    //}
+   
 
     private void OnDisable()
     {
