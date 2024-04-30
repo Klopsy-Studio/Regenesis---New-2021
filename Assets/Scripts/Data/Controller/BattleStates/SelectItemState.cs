f4ccf59266282f9251ccc349e0b80372f678004d
Aura <darsodios@gmail.com>
2024-05-01 01:36:29 +0200

    no message

--- Assets/Scripts/Data/Controller/BattleStates/SelectItemState.cs
+++ Assets/Scripts/Data/Controller/BattleStates/SelectItemState.cs
@@ -43,7 +43,6 @@ public class SelectItemState : BattleState
         {
             var item = itemList[i];
 
-            owner.itemSelectionUI.parent[i].gameObject.SetActive(true);
             owner.itemSelectionUI.options[i].GetComponent<SelectorMovement>().controller = owner;
             owner.itemSelectionUI.options[i].GetComponent<TextMeshProUGUI>().text = item.consumable.itemName;
             owner.itemSelectionUI.itemAmountText[i].GetComponent<TextMeshProUGUI>().text = item.amount.ToString();
